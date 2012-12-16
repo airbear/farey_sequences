43d5691d66f95b949f75d568b548628844798747
hordecore <hordecore@oleg.(none)>
2012-12-16 14:03:24 +0600

    Говно ебаное

--- fareyseq.c
+++ fareyseq.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 
-/*
- * вспомогательная, считает фи энные(ф-ия эйлера)
- */
-unsigned long long int fi(int n) {
+static double K1 = 0;
+static double K2 = 0;
+
+struct farey {
+	unsigned int up;
+	unsigned int down;
+};
+
+/* вспомогательная, считает фи энные(ф-ия эйлера) */
+unsigned long long int fi(int n)
+{
 	//короче так. юзаем решето эратосфена, ищем все простые от 1 до n и потом проверяем
 	unsigned int prime[n];
 	unsigned long int i;
 	unsigned long int j;
-	prime[0]=0;
-	prime[1]=0;
+	prime[0] = 0;
+	prime[1] = 0;
+	for (i = 2; i <= n; i++) {
+		prime[i] = 1;
+	}
 
-	for(i = 2; i <= n; i++)
-		prime[i]=1;
-
-	for (i = 2; i<=n; ++i)
+	for (i = 2; i <= n; ++i)
 		if (prime[i])
 			if (i * i <= n)
-				for (j=i*i; j<=n; j+=i)
-					prime[j]=0;
-
+				for (j = i * i; j <= n; j += i) {
+					prime[j] = 0;
+				}
 	//теперь ещё те выкинем, которые не делители
-	for(i = 0; i <= n; i++) {
-		if(prime[i])
-			if(n % i != 0)
-				prime[i]=0;
+	for (i = 0; i <= n; i++) {
+		if (prime[i])
+			if (n % i != 0) {
+				prime[i] = 0;
+			}
 	}
 
 	//получили что-то вроде массива нужных делителей, теперь считаем
-	unsigned long long int f = 1;
-	for(i = 0; i <=n; i++)
-		if (prime[i]) {
-			printf("%llu *= 1 - 1/%lu\n", f, i);
-			f *= 1 - 1/i;
-		}
+	double f = 1;
+	for (i = 0; i <= n; i++)
+		if (prime[i])
+			f *= 1.0 - 1.0 / i;
 	f *= n;
-	return f;
+	return (long long int)f;
+}
+
+/* Вспомогательная для K1 / K2 */
+double dkn(farey *q, int k, unsigned long long int A)
+{
+	return ((q->up * A - k * q->down) / (A * 1.0)) / q->down;
 }
 
 /*
@@ -44,48 +56,77 @@ unsigned long long int fi(int n) {
  * тащем-то не нужна, также как и следущая, ибо количество элементов в последовательности
  * можно узнать тупо из размера эррэй листа, но пусть будет, мало ли что 
  */
-unsigned long long int elem_count(int order) {
+unsigned long long int elem_count(int order)
+{
 	unsigned long long int s = 0;
 	unsigned int i;
 	for (i = 1; i <= order; i++)
-		s+=fi(i);
+		s += fi(i);
 	return s;
 }
 
 
-int main(void) {
-	int i;
-	for (i = 0; i < 10; i++)
-		fi(i);
-	return 0;
+
+void magic(int n)
+{
+	int i,j;
+	struct farey current[1];
+	current[0].up = 1;
+	current[0].down = 1;
+	for (i = 2; i < n; i++) {
+		struct farey last[i-1];
+		for (j = 0; j < i - 1; j++) 
+			last[j] = current[j];
+		
+			
+	}
+
+	return;
 }
 
-#if 0
+
+
 /*
  * строит последовательность фарея
  * order - порядок, до которого нужно построить
  * start - последовательность, с которой начать строить. мб и не надо
  * напока мне кажется, что так будет побыстрее на оче больших порядках
  */
-public static ArrayList<Farey> getFareySeq(ArrayList<Farey> start, int order) {
-	Farey tmp1,tmp2;
-	ArrayList<Farey> current = new ArrayList<Farey>(start);
+
+/* 
+ * 1. Берем массив current (то от чего отталкиваемся, т.е. start)
+ * 2. Узнаем порядок переданной последовательности (order / 10)
+ * 3. В цикле до указанного переданного порядка (order)
+ * 4. Меняем местами last и current 
+ */
+
+
+
+#if 0
+public static ArrayList < Farey > getFareySeq(ArrayList < Farey > start,
+					      int order)
+{
+	Farey tmp1, tmp2;
+	ArrayList < Farey > current = new ArrayList < Farey > (start);
 	//построение до указанного в order порядка:
 	//надо узнать, какой порядок был у переданной последовательности старт:
-	int order_start = start.get(start.size()-1).down; //ибо там 1/n
-	for (int i = order_start +1; i <= order; i++) {
-		if(current.isEmpty()) System.out.println("wtf bro current is empty");
-		ArrayList<Farey> last = new ArrayList<Farey>(current);
+	int order_start = start.get(start.size() - 1).down;	//ибо там 1/n
+	for (int i = order_start + 1; i <= order; i++) {
+		ArrayList < Farey > last = new ArrayList < Farey > (current);
 		current.clear();
 		//отдельно берем первую дробь из последовательности, сразу удаляем из списка и кладем в current
-		tmp1 = last.get(0); last.remove(0);
+		tmp1 = last.get(0);
+		last.remove(0);
 		current.add(tmp1);
-		while (! last.isEmpty()) {
-			tmp1 = last.get(0); last.remove(0);
-			if((tmp1.down + current.get(current.size()-1).down) <= i) {
+		while (!last.isEmpty()) {
+			tmp1 = last.get(0);
+			last.remove(0);
+			if ((tmp1.down +
+			     current.get(current.size() - 1).down) <= i) {
 				//впихиваем между ними медианту:
-				tmp2 = current.get(current.size()-1);
-				Farey tmp3 = new Farey(tmp1.up + tmp2.up, tmp1.down + tmp2.down);
+				tmp2 = current.get(current.size() - 1);
+				Farey tmp3 = new Farey(tmp1.up + tmp2.up,
+						       tmp1.down + tmp2.down);
 				current.add(tmp3);
 				current.add(tmp1);
 			} else {
@@ -104,30 +145,36 @@ public static ArrayList<Farey> getFareySeq(ArrayList<Farey> start, int order) {
 	 */
 	return current;
 }
+
 /*
  * some exp shit
  * 
  */
 
 
-static double dkn(Farey q, int k, double A) {
-	return (q.up * A - k*q.down)/A/q.down;
-}
 
-static double K1(ArrayList<Farey> F) {
+
+#endif
+
+#if 0
+static double K2(ArrayList < Farey > F)
+{
 	double sum = 0;
-	for(int i = 0; i < F.size(); i++) {
-		int k = F.size()-1-i;
-		sum += dkn(F.get(k),i+1,F.size()) * dkn(F.get(k),i+1,F.size());
+	for (int i = 0; i < F.size(); i++) {
+		int k = F.size() - 1 - i;
+		sum += Math.abs(dkn(F.get(k), i + 1, F.size()));
 	}
 	return sum;
 }
 
-static double K2(ArrayList<Farey> F) {
+static double K1(ArrayList < Farey > F)
+{
 	double sum = 0;
-	for(int i = 0; i < F.size(); i++) {
-		int k = F.size()-1-i;
-		sum += Math.abs(dkn(F.get(k),i+1,F.size()));
+	for (int i = 0; i < F.size(); i++) {
+		int k = F.size() - 1 - i;
+		sum +=
+		    dkn(F.get(k), i + 1, F.size()) * dkn(F.get(k), i + 1,
+							 F.size());
 	}
 	return sum;
 }
